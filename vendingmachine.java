e6855b658b217f25b4fe0ca5d8e47c1850c542a4
26785 <43673104+26785@users.noreply.github.com>
2018-11-09 11:12:09 +0100

    Add files via upload
    ints gemaakt + array lists

--- vendingmachine.java
+++ vendingmachine.java
@@ -4,6 +4,24 @@ public class vendingmachine {
     public static void main(String[]args ){
         Scanner scanner = new Scanner(System.in);
 
+        int cola = 90;
+        int sprite = 90;
+        int fanta = 90;
+        int drpepper = 90;
+        int icetea = 90;
+        int user = 200;
+        int choice = 0;
+        int money = 0;
+        int amount = 0;
+
+        ArrayList<String> drankjes = new ArrayList<String>();
+        drankjes.add("cola");
+        drankjes.add("sprite");
+        drankjes.add("fanta");
+        drankjes.add("drpepper");
+        drankjes.add("icetea");
+
+        ArrayList<String> output = new ArrayList<String>();
         
     }
 
